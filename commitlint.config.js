66e9869e80a2aa9a45300da12c50c0eaba79248c
Adrian Schmidt <adrian.schmidt@lime.tech>
2019-11-21 13:37:32 +0100

    Revert "feat(config): update config to match existing config in lime-elements"
    This reverts commit 562b31e28fbaa12876a3f3ed85ea20ea51b8de68.

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,24 +1,3 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
-  rules: {
-    'header-max-length': [2, 'always', 100],
-    'scope-case': [0, 'always', 'lower-case'],
-    'type-enum': [
-      2,
-      'always',
-      [
-        'feat',
-        'fix',
-        'docs',
-        'style',
-        'refactor',
-        'perf',
-        'test',
-        'build',
-        'ci',
-        'chore',
-        'revert',
-      ],
-    ],
-  },
 }
